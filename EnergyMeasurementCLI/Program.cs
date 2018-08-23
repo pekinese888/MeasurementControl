14e39c9225caa7c2cc89a76b70bf0605ff95ce6c
Kevin Kötz <kevinkoetz@gmail.com>
2018-08-23 19:06:43 +0200

    Just trying some things

--- EnergyMeasurementCLI/Program.cs
+++ EnergyMeasurementCLI/Program.cs
@@ -11,14 +11,20 @@ namespace EnergyMeasurementCLI
     {
         static void Main(string[] args)
         {
-            //Example for Message Based Communication
+            //Example for Message Based Communication with TCPIP0::192.168.1.7::inst0::INSTR
             ResourceManager resourceManager = new ResourceManager();
-            IEnumerable<string> TcpipInstruments = resourceManager.Find("TCPIP?*INSTR");
+            IEnumerable<string> TcpipInstruments = resourceManager.Find("?*INSTR");
+            foreach (string str in TcpipInstruments)
+            {
+                Console.WriteLine(str);
+            }
             TcpipSession session = new TcpipSession(TcpipInstruments.First());
             session.FormattedIO.WriteLine("FETC?V,I,W");
             Console.WriteLine(session.FormattedIO.ReadString());
             session.Dispose();
             resourceManager.Dispose();
+
+
         }
     }
 }
